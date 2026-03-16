ddf69928445db3edd1f2ca01b56ce92ceb017ccc
Andrew Derbenev <58420537+Andrewd226@users.noreply.github.com>
2026-03-17 03:26:45 +0500

    update models

--- digital_store/settings.py
+++ digital_store/settings.py
@@ -57,12 +57,11 @@ INSTALLED_APPS = [
     'oscar.apps.checkout.apps.CheckoutConfig',
     'oscar.apps.address.apps.AddressConfig',
     'oscar.apps.shipping.apps.ShippingConfig',
-    'oscar.apps.catalogue.apps.CatalogueConfig',
+    # 'oscar.apps.catalogue.apps.CatalogueConfig',
     'catalogue.apps.CatalogueConfig',                   # ← наш форк
     'oscar.apps.catalogue.reviews.apps.CatalogueReviewsConfig',
-    # 'catalogue.reviews.apps.CatalogueReviewsConfig',    # ← наш форк
     'oscar.apps.communication.apps.CommunicationConfig',
-    'oscar.apps.partner.apps.PartnerConfig',
+    # 'oscar.apps.partner.apps.PartnerConfig',
     'partner.apps.PartnerConfig',                       # ← наш форк
     'oscar.apps.basket.apps.BasketConfig',
     'oscar.apps.payment.apps.PaymentConfig',
