425ef689cc076d71db56f4e186604b633c7f6471
Andrew Derbenev <58420537+Andrewd226@users.noreply.github.com>
2026-03-21 16:31:23 +0500

    fix partner config

--- partner/apps.py
+++ partner/apps.py
@@ -1,3 +1,6 @@
+from oscar.apps.partner.apps import PartnerConfig as OscarPartnerConfig
+
+
 class PartnerConfig(OscarPartnerConfig):
     name = "partner"
     default_auto_field = "django.db.models.BigAutoField"
