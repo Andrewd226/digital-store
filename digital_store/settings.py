7fc104d4fbcda86a83af56f49f331f9d2f1609b5
Andrew Derbenev <58420537+Andrewd226@users.noreply.github.com>
2026-03-28 05:37:37 +0500

    add gunicorn

--- digital_store/settings.py
+++ digital_store/settings.py
@@ -107,6 +107,7 @@ HAYSTACK_CONNECTIONS = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -213,10 +214,10 @@ SALT_KEY = env("SALT_KEY")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
