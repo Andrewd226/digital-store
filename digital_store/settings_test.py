049486b4d2fc4bc1b2fcf6fbe5900f08fd6b6280
Andrew Derbenev <58420537+Andrewd226@users.noreply.github.com>
2026-03-29 19:42:42 +0500

    update settings

--- digital_store/settings_test.py
+++ digital_store/settings_test.py
@@ -4,4 +4,18 @@ digital_store/settings_test.py
 import os
 os.environ["ENV_FOR_DYNACONF"] = "tests.local"
 
-from digital_store.settings import *  # noqa
+# from digital_store.settings import *  # noqa
+
+
+from pathlib import Path
+from dynaconf import Dynaconf
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+_settings = Dynaconf(
+    settings_file=BASE_DIR / "settings.yaml",
+    environments=True,
+    env_switcher="ENV_FOR_DYNACONF",
+)
