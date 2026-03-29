0576713b730d8bd4c6481d107cca126f30481972
Andrew Derbenev <58420537+Andrewd226@users.noreply.github.com>
2026-03-29 19:36:12 +0500

    update settings

--- digital_store/settings_test.py
+++ digital_store/settings_test.py
@@ -1,27 +1,7 @@
 """
 digital_store/settings_test.py
 """
-from pathlib import Path
-
-import environ
+import os
+os.environ["ENV_FOR_DYNACONF"] = "tests.local"
 
 from digital_store.settings import *  # noqa
-
-# ─── Environment ──────────────────────────────────────────────────────────────
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env(BASE_DIR / ".env")
-
-# ─── Database (Test) ──────────────────────────────────────────────────────────
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("TEST_DB_NAME"),
-        "USER": env("TEST_DB_USER"),
-        "PASSWORD": env("TEST_DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
-    }
-}
