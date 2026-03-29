e9b59ceb6c48ab01a5d9a9d8867bd51fb01d37c7
Andrew Derbenev <58420537+Andrewd226@users.noreply.github.com>
2026-03-30 01:18:29 +0500

    fix float to decimal

--- helpers/arithmetic.py
+++ helpers/arithmetic.py
@@ -6,4 +6,4 @@ from decimal import ROUND_DOWN, Decimal
 
 def round_decimal(value: Decimal, exp: int, rounding: str = ROUND_DOWN) -> Decimal:
     exp = Decimal('10') ** (-exp)
-    return value.quantize(exp, rounding=rounding_config.rounding)
+    return value.quantize(exp, rounding=rounding)
